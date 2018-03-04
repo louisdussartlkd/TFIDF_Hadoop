1733d037587e9ddbf0ff1b08fe1f8693eefdb204
SebastianS09 <30629645+SebastianS09@users.noreply.github.com>
2018-03-04 15:53:50 +0100

    same

--- rs_mapper.py
+++ rs_mapper.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python
 
 import sys
-from random import randint
 import string
 import re
 
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
-    # remove leading and trailing whitespace and punctuation
+    # remove leading and trailing whitespace, punctuation and caps
     line = line.strip()
     line = re.sub('['+string.punctuation+']', '', line)
+    line = line.lower()
 
     # split the line into words
     words = line.split()
-    # increase counters
+
     for word in words:
-    #add a random number between 0 and 10000 as a key
-        print '%s\t%s' % (word, randint(0,1000))
+        print '%s\t%s' % (word, 1)
